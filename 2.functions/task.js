898daa3c821779f70fb2ad36db9a5e65a23aa366
Vladimir Kovalenok <vladimskv@yandex.ru>
2022-04-08 00:30:37 +0300

    добавлены решения задач 1, 2 и 3

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,68 @@
+"use strict";
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
-
-  // Ваш код
-
+  min = Infinity;
+  max = -Infinity;
+  sum = 0;
+  let i = 0;
+  while (i < arr.length) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    sum += arr[i];
+    i++;
+  }
+  
+  avg = +((sum / arr.length).toFixed(2));
+  
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
   let sum;
-
-  // Ваш код
-
+  sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
   let max;
-
-  // Ваш кода
-  // for ...
+  let sum;
+  max = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    sum = func(arrOfArr[i]);
+    if (sum > max) {
+      max = sum;
+    }
+  }
   
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min, max, sum;
+  min = Infinity;
+  max = -Infinity;
+  sum = 0;
+  let i = 0;
+  while (i < arr.length) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    i++;
+  }
+  
+  return (Math.abs(max - min));
 }
