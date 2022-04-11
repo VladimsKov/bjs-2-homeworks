259115995f7a5a874540549841d60a6fe5417bcf
Vladimir Kovalenok <vladimskv@yandex.ru>
2022-04-12 00:28:04 +0300

    добавлены решения двух задач

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,24 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  
+  function isEqual(item1) {
+    return item1 === arr2[arr1.indexOf(item1)];
+  }
+  
+  if (arr1.length !== arr2.length) {
+    result = false;
+  } else {
+    result = arr1.every(isEqual);
+  }
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
+  
+  resultArr = arr.filter(item => item > 0)
+  .filter(item => item % 3 === 0)
+  .map(item => item * 10);
+  
   return resultArr; // array
 }
