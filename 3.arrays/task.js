eb0e01bd4a79dbb393937dd2d761715791a012ab
Vladimir Kovalenok <vladimskv@yandex.ru>
2022-04-12 23:19:23 +0300

    добавлено короткое решение

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,18 @@
 function compareArrays(arr1, arr2) {
   let result;
+  //более короткое решение
+  result = (arr1.length === arr2.length) && (arr1.every(item => item === arr2[arr1.indexOf(item)]));
   
-  function isEqual(item1) {
+  
+  /*function isEqual(item1) {
     return item1 === arr2[arr1.indexOf(item1)];
-  }
+  }*/
   
-  if (arr1.length !== arr2.length) {
+  /*if (arr1.length !== arr2.length) {
     result = false;
   } else {
     result = arr1.every(isEqual);
-  }
+  }*/
   return result; // boolean
 }
 
